8c2273382e2cf43707498330c03f54583934a358
starryCcchj <1640448282@qq.com>
2021-10-25 09:12:30 +0800

    frontend#001 -login and register

---
+++ FrontEnd/src/views/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">
+      <el-menu :default-openeds="['1', '2']">
+      <el-submenu index="1">
+        <template slot="title"><i class="el-icon-message"></i>导航一</template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        
+        <el-submenu index="1-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="1-4-1">选项4-1</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title"><i class="el-icon-menu"></i>导航二</template>
+        <el-menu-item-group>
+          <template slot="title">分组一</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        
+      </el-submenu>
+      
+    </el-menu>
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+
+  }
+</style>
---
+++ FrontEnd/src/views/Register.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="register-container">
+      <el-form :model="ruleForm2" :rules="rules2"
+        status-icon
+        label-position="left" 
+        label-width="5px"
+        class="register-page"
+      >
+        <h3 class="title">注册新账号</h3>
+        <el-form-item prop="username">
+                <el-input type="text" 
+                    v-model="ruleForm2.username" 
+                    auto-complete="off" 
+                    placeholder="用户名"
+                ></el-input>
+            </el-form-item>
+                <el-form-item prop="password">
+                    <el-input type="password" 
+                        v-model="ruleForm2.password" 
+                        auto-complete="off" 
+                        placeholder="密码"
+                    ></el-input>
+                </el-form-item>
+            <el-form-item prop="email">
+              <el-row :gutter="4">
+              <el-col :span="16">
+                <el-input type="email" 
+                    v-model="ruleForm2.email" 
+                    auto-complete="off" 
+                    placeholder="电子邮箱"
+                   
+                ></el-input>
+              </el-col>
+              <el-col :span="5" >
+                <el-button plain>发送验证码</el-button>
+              </el-col>
+              </el-row>
+                </el-form-item>
+                <el-form-item prop="verification">
+              <el-form-item prop="verification">
+                <el-input type="verification" 
+                    v-model="ruleForm2.verification" 
+                    auto-complete="off" 
+                    placeholder="验证码"
+                ></el-input>
+              </el-form-item>
+                </el-form-item>
+                 <el-form-item style="width:100%;">
+                <el-button type="primary" style="width:100%;" @click="Register" >确认注册</el-button>
+            </el-form-item>
+      </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'register',
+    data(){
+        return {
+        
+            ruleForm2: {
+                username: '',
+                password: '',
+                email:'',
+                verification:''
+            },
+            rules2: {
+                username: [{min:4,max:10,required: true, message: '你的用户名长度应在4到10个字符之间', trigger: 'blur'}],
+                password: [{min:8,max:20,required: true, message: '你的密码长度应该在8到20位之间', trigger: 'blur'}],
+                email:[{required: true, message: '请输入邮箱地址',trigger: 'blur'}, {validator:function(rule,value,callback){
+                   
+                          if (value !== '') { 
+                            // var reg=/^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+                            var reg=/^[A-Za-z0-9]+([_.][A-Za-z0-9]+)*@([A-Za-z0-9-]+\.)+[A-Za-z]{2,6}$/;
+                              if(!reg.test(value)){
+                                  callback(new Error('请输入有效的邮箱'));
+                             }
+                             else{}
+                  }
+    	      }}],
+                verification:[{required: true, message: '请输入邮箱中接收到的验证码', trigger: 'blur'}]
+            },
+            checked: false
+        }
+    },
+    methods: {
+       Register()
+       {
+           alert("111");
+       },
+       
+    },
+   
+}
+</script>
+
+<style>
+.register-container {
+    width: 100%;
+    height: 100%;
+}
+.register-page {
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    margin: 180px auto;
+    width: 350px;
+    padding: 35px 35px 15px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+}
+</style>
